9484d4bbebdbe6964a132c85d87b00cf19dd6b1c
kai5koson <jason.jako@my-digital-school.org>
2025-04-25 11:27:47 +0200

    Ajout de la configuration CORS pour Vercel

--- api/index.php
+++ api/index.php
@@ -4,10 +4,11 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Configuration CORS améliorée - IMPORTANT : Ces en-têtes doivent être envoyés avant tout contenu
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: https://ap-ialerte.vercel.app');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, X-Requested-With');
+header('Access-Control-Max-Age: 86400'); // 24 heures
 
 // Répondre immédiatement aux requêtes OPTIONS (preflight)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
